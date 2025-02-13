d269a462ec696a93174f2419a28894fd5f839622
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2025-02-13 20:27:27 -0300

    carrossel - ajustes

--- carrossel.js
+++ carrossel.js
@@ -1,5 +1,5 @@
 let index = 0; // Índice da imagem atual
-const items = document.querySelectorAll('.carrossel-item'); // Seleciona todos os itens do carrossel
+const items = document.querySelectorAll('.carrossel-container'); // Seleciona todos os itens do carrossel
 const totalItems = items.length; // Total de imagens no carrossel
 
 
@@ -9,8 +9,8 @@ function mudarImagem(direcao) {
   index = (index + direcao + totalItems) % totalItems; // Garante que o índice não saia do intervalo
 
   // Move o carrossel para a posição da imagem desejada
-  const container = document.querySelector('.carrossel-container');
-  container.style.transform = `translateX(-${index * 100}%)`;
+  const container = document.querySelector('.carrossel');
+  container.style.transform = `translateX(-${index * 10}%)`;
 
 
 }
