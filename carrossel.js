d35ce32894e6005ebf98fe097ec4db5b064ea932
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2025-02-20 20:10:54 -0300

    ajustando segundo carrossel

--- carrossel.js
+++ carrossel.js
@@ -2,6 +2,10 @@ let index = 0; // Índice da imagem atual
 const items = document.querySelectorAll('.carrossel-container'); // Seleciona todos os itens do carrossel
 const totalItems = items.length; // Total de imagens no carrossel
 
+let index1 = 0;
+const items1 = document.querySelectorAll('.carrossel2-container-all');
+const totalItems1 = items1.length; 
+
 
 // Função para mudar a imagem
 function mudarImagem(direcao) {
@@ -10,7 +14,14 @@ function mudarImagem(direcao) {
 
   // Move o carrossel para a posição da imagem desejada
   const container = document.querySelector('.carrossel');
-  container.style.transform = `translateX(-${index * 10}%)`;
+  container.style.transform = `translateX(-${index * 40}%)`;
 
+}
 
+function mudarImagem1(direcao) {
+
+  index1 = (index1 + direcao + totalItems1) % totalItems1;
+
+  const container = document.querySelector('.carrossel2-container');
+  container.style.transform = `translateX(-${index1 * 30}%)`;
 }
