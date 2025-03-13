081fed45c32da6a591df38f3f86c7033bd9e037b
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2025-03-13 19:36:10 -0300

    copia netshoe primeiros passos

--- carrossel.js
+++ carrossel.js
@@ -1,41 +1,3 @@
-// let index = 0; // Índice da imagem atual
-// const items = document.querySelectorAll('.carrossel-container'); // Seleciona todos os itens do carrossel
-// const totalItems = items.length; // Total de imagens no carrossel
-
-// let index2 = 0;
-// const items2 = document.querySelectorAll('.carrossel2-container');
-// const totalItems2 = items2.length; 
-
-// let index3 = 0;
-// const items3 = document.querySelectorAll('.carrossel3-container');
-// const totalItems3 = items3.length; 
-
-// // Função para mudar a imagem
-// function mudarImagem(direcao) {
-//   // Atualiza o índice com base na direção
-//   index = (index + direcao + totalItems) % totalItems; // Garante que o índice não saia do intervalo
-
-//   // Move o carrossel para a posição da imagem desejada
-//   const container = document.querySelector('.carrossel');
-//   container.style.transform = `translateX(-${index * 30}%)`;
-// }
-
-// function mudarImagem2(direcao) {
-
-//   index2 = (index2 + direcao + totalItems2) % totalItems2;
-
-//   const container = document.querySelector('.carrossel2');
-//   container.style.transform = `translateX(-${index2 * 10}%)`;
-// }
-
-// function mudarImagem3(direcao) {
-
-//   index3 = (index3 + direcao + totalItems3) % totalItems3;
-
-//   const container = document.querySelector('.carrossel3');
-//   container.style.transform = `translateX(-${index3 * 10}%)`;
-// }
-
 let index = 0;
 const carrosselContainer = document.querySelector('.carrossel'); 
 const totalItems = document.querySelectorAll('.carrossel-container').length;
@@ -115,4 +77,109 @@ function atualizarCarrossel3() {
 function mudarImagem3(direcao) {
   index3 = (index3 + direcao + totalItems3) % totalItems3;
   atualizarCarrossel3();
+}
+
+let index4 = 0;
+const carrossel4Container = document.querySelector('.carrossel4'); 
+const totalItems4 = document.querySelectorAll('.carrossel4-container').length;
+const itemWidth4 = 10;
+
+function atualizarCarrossel4() {
+  const translateValue = -(index4 * itemWidth4);
+
+  carrossel4Container.style.transform = `translateX(${translateValue}%)`;
+
+  if (translateValue <= -50) {
+
+    index4 = 0;
+    carrossel4Container.style.transition = 'none';
+    carrossel4Container.style.transform = `translateX(0%)`;
+    setTimeout(() => {
+      carrossel4Container.style.transition = 'transform 0.5s ease';
+    }, 100);
+  }
+}
+
+function mudarImagem4(direcao) {
+  index4 = (index4 + direcao + totalItems4) % totalItems4;
+  atualizarCarrossel4();
+}
+
+let index5 = 0;
+const carrossel5Container = document.querySelector('.carrossel5'); 
+const totalItems5 = document.querySelectorAll('.carrossel5-container').length;
+const itemWidth5 = 10;
+
+function atualizarCarrossel5() {
+  const translateValue = -(index5 * itemWidth5);
+
+  carrossel5Container.style.transform = `translateX(${translateValue}%)`;
+
+  if (translateValue <= -50) {
+
+    index5 = 0;
+    carrossel5Container.style.transition = 'none';
+    carrossel5Container.style.transform = `translateX(0%)`;
+    setTimeout(() => {
+      carrossel5Container.style.transition = 'transform 0.5s ease';
+    }, 100);
+  }
+}
+
+function mudarImagem5(direcao) {
+  index5 = (index5 + direcao + totalItems5) % totalItems5;
+  atualizarCarrossel5();
+}
+
+
+let index6 = 0;
+const carrossel6Container = document.querySelector('.carrossel6'); 
+const totalItems6 = document.querySelectorAll('.carrossel6-container').length;
+const itemWidth6 = 10;
+
+function atualizarCarrossel6() {
+  const translateValue = -(index6 * itemWidth6);
+
+  carrossel6Container.style.transform = `translateX(${translateValue}%)`;
+
+  if (translateValue <= -50) {
+
+    index6 = 0;
+    carrossel6Container.style.transition = 'none';
+    carrossel6Container.style.transform = `translateX(0%)`;
+    setTimeout(() => {
+      carrossel6Container.style.transition = 'transform 0.5s ease';
+    }, 100);
+  }
+}
+
+function mudarImagem6(direcao) {
+  index6 = (index6 + direcao + totalItems6) % totalItems6;
+  atualizarCarrossel6();
+}
+
+let index7 = 0;
+const carrossel7Container = document.querySelector('.carrossel7'); 
+const totalItems7 = document.querySelectorAll('.carrossel7-container').length;
+const itemWidth7 = 10;
+
+function atualizarCarrossel7() {
+  const translateValue = -(index7 * itemWidth7);
+
+  carrossel7Container.style.transform = `translateX(${translateValue}%)`;
+
+  if (translateValue <= -50) {
+
+    index7 = 0;
+    carrossel7Container.style.transition = 'none';
+    carrossel7Container.style.transform = `translateX(0%)`;
+    setTimeout(() => {
+      carrossel7Container.style.transition = 'transform 0.5s ease';
+    }, 100);
+  }
+}
+
+function mudarImagem7(direcao) {
+  index7 = (index7 + direcao + totalItems7) % totalItems7;
+  atualizarCarrossel7();
 }
